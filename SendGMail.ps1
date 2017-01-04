cd8ef96343e35f70aa2322e643c39142a9b9c6e1
Alvin <alvin.vaughn@outlook.com>
2017-01-04 15:07:30 -0600

    Updated SendGMail script

--- SendGMail.ps1
+++ SendGMail.ps1
@@ -1,38 +1,24 @@
-﻿#+-------------------------------------------------------------------+   
-#| = : = : = : = : = : = : = : = : = : = : = : = : = : = : = : = : = |   
-#|{>/-------------------------------------------------------------\<}|            
-#|: | Author:  Aman Dhally                                        | :|            
-#| :| Email:   amandhally@gmail.com 
-#|: | Purpose: Smart Backup and create folder by Date        
-#| :|           
-#|: |                                                        
-#| :|           
-#|: |                 Date: 29 November 2011  
-#|: |                             
-#| :|     /^(o.o)^\    Version: 1                                    |: |  
-#|{>\-------------------------------------------------------------/<}| 
-#| = : = : = : = : = : = : = : = : = : = : = : = : = : = : = : = : = | 
-#+-------------------------------------------------------------------+ 
- 
- 
+﻿
 #System Variable for backup Procedure 
  
  $date = Get-Date -Format d-MMMM-yyyy 
  
+ # $destination = '\\vfs04\Users$\vaugha1\Documents'
   
 #Email Variables 
  
  $smtp = "smtp.gmail.com"
  $smtpPort = "587" 
  $from = "IITG Script Email <advtalk28@gmail.com>" 
- $to = "Alvin Vaughn <alvin.vaughn@outlook.com>" 
- $body = "Log File of N drive database bacupk is attached for: $date" 
- $subject = "Backup on $date" 
+ $to = "Alvin Vaughn <alvin.vaughn@harrishealth.org>" 
+ $body = "Test Log: $date" 
+ $subject = "Test Log Capture Email on $date" 
   
 # Backup Process started  
-            $backup_log = Dir -Recurse $destination | out-File "$destination\backup_log.txt" 
-            $attachment = "$destination\backup_log.txt" 
+            #$backup_log = Dir -Recurse $destination | out-File "$destination\backup_log.txt" 
+  #          $attachment = "$destination\APTMKTDATA.cer" 
+   # -Attachments $attachment
 
 #Send an Email to User  
-  send-MailMessage -SmtpServer $smtp -port $smtpPort -UseSsl -From $from -To $to -Credential (Get-credential) -Subject $subject -Attachments $attachment -Body $body -BodyAsHtml  
+  send-MailMessage -SmtpServer $smtp -port $smtpPort -UseSsl -From $from -To $to -Credential (Get-credential) -Subject $subject  -Body $body -BodyAsHtml  
  
